e9fb277891a435827ae207eef6e8dddf6e49e3ae
Ihor Cherniavskiy <chernyavskiy2019ios@gmail.com>
2022-09-21 01:15:38 +0300

    Code after refactoring

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,32 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Header from './components/header/header'
-import Registeration from './components/authorization/authorization'
-import Main from './components/main/main'
-import Account from './components/account/account'
-import Forgot from './components/forgot/forgot'
-import MoviebaseAnimatedLogo from './components/animated-logo/animated-logo'
-import Film from './components/film/film'
+import Header from './components/header/Header'
+import Registeration from './components/authorization/Authorization'
+import Main from './components/main/Main'
+import Account from './components/account/Account'
+import Forgot from './components/forgot/Forgot'
+import { MoviebaseAnimatedLogo } from './components/animated-logo/AnimatedLogo'
+import Film from './components/film/Film'
 
-function App(): JSX.Element {
-
-  const [isFirstTimeOpen, setIsFirstTimeOpen] = useState(true);
+function App (): JSX.Element {
+  const [isFirstTimeOpen, setIsFirstTimeOpen] = useState(true)
 
   useEffect(() => {
-    setTimeout(() => {
-      setIsFirstTimeOpen(false);
+    const t = setTimeout(() => {
+      setIsFirstTimeOpen(false)
     }, 2500)
+    return () => clearTimeout(t)
   }, [isFirstTimeOpen])
 
   return (
     <div className='App'>
       <BrowserRouter>
-        {isFirstTimeOpen ?
-          <div className="moviebase-greeting-block">
-            <MoviebaseAnimatedLogo />
-          </div>
-          : ""}
+        {isFirstTimeOpen
+          ? (
+              <div className="moviebase-greeting-block">
+              <MoviebaseAnimatedLogo />
+              </div>
+            )
+          : ''}
         <Header></Header>
 
         <Routes>
