a5876786b6e37b715f7d5374264c9261e6681b23
Ihor Cherniavskyi <chernyavskiy2019ios@gmail.com>
2023-01-10 00:02:57 +0200

    Added footer component

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
+import miniLogo from '../../assets/bigLogo.svg'
+import InstagramIcon from '@mui/icons-material/Instagram'
+import FacebookIcon from '@mui/icons-material/Facebook'
+import LinkIcon from '@mui/icons-material/Link'
+import GitHubIcon from '@mui/icons-material/GitHub'
 
 export const Footer: React.FC = () => {
   return (
@@ -7,7 +12,7 @@ export const Footer: React.FC = () => {
       sx={{
         padding: '30px',
         width: 'calc(100% - 60px)',
-        height: '100px',
+        height: '150px',
         display: 'flex',
         alignItems: 'center',
         position: 'absolute',
@@ -15,8 +20,49 @@ export const Footer: React.FC = () => {
         backgroundColor: '#30302f',
       }}
     >
-      Footer
+      <Box sx={{ display: 'flex', gap: '5px' }}>
+        <Typography variant='body1'>©2022 </Typography>
+        <Typography variant='body1' sx={{ fontWeight: 'bold' }}>
+          <a href='https://www.moviebase.com.ua' style={{ textDecoration: 'none', color: 'white' }}>
+            MOVIEBASE
+          </a>
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          height: '100%',
+          justifySelf: 'center',
+          margin: 'auto',
+          cursor: 'default',
+          display: 'flex',
+          alignContent: 'center'
+        }}
+      >
+        <img src={miniLogo} alt='moviebaseIcon' style={{ height: '50%', margin: 'auto' }} />
+      </Box>
+      <Box sx={{ display: 'flex', gap: '5px' }}>
+        <a
+          href='https://www.instagram.com/f3anofficial'
+          style={{ textDecoration: 'none', color: 'white' }}
+        >
+          <InstagramIcon fontSize='large' />
+        </a>
+        <a
+          href='https://www.facebook.com/f3anofficial'
+          style={{ textDecoration: 'none', color: 'white' }}
+        >
+          <FacebookIcon fontSize='large' />
+        </a>
+        <a
+          href='https://www.ihorcherniavskyi.com.ua'
+          style={{ textDecoration: 'none', color: 'white' }}
+        >
+          <LinkIcon fontSize='large' />
+        </a>
+        <a href='https://github.com/f3an' style={{ textDecoration: 'none', color: 'white' }}>
+          <GitHubIcon fontSize='large' />
+        </a>
+      </Box>
     </Box>
   )
 }
-  
