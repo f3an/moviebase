966d3ddf9b2f6c63765e366fe64fc9c8209170ea
Ihor Cherniavskyi <chernyavskiy2019ios@gmail.com>
2023-02-13 00:19:47 +0200

    Fixed AutoPlay Slider

--- src/app/components/mainPage/mainSlider.tsx
+++ src/app/components/mainPage/mainSlider.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Box, IconButton } from '@mui/material'
 import { ArrowBack, ArrowForward } from '@mui/icons-material'
 import styled from 'styled-components'
@@ -7,30 +7,36 @@ import { useShownMovies } from '../../hooks/useShownMovies'
 export const MainSlider: React.FC<Props> = ({ movies, focusedMovie, setFocusedMovie }) => {
   const focusedMovieIndex = movies.findIndex((movie) => movie === focusedMovie)
   const imageApiUrl = process.env.REACT_APP_TMDB_IMAGE_URL ?? ''
-
+  const [isAutoPlayStoped, setIsAutoPlayStoped] = useState(false)
   const shownMovies = useShownMovies({ focusedMovieIndex, movies })
 
   useEffect(() => {
-    const autoPlay = setTimeout(() => {
+    const play = () => {
       focusedMovieIndex < 19
         ? setFocusedMovie(movies[focusedMovieIndex + 1])
         : setFocusedMovie(movies[0])
-    }, 5000)
-
-    const stopAutoPlay = () => {
-      clearTimeout(autoPlay)
     }
 
-    return stopAutoPlay()
-  }, [setFocusedMovie, focusedMovieIndex, movies])
+    const autoPlay = setTimeout(play, 5000)
+
+    const stopAutoPlay = () => {
+      if (isAutoPlayStoped) {
+        clearTimeout(autoPlay)
+      }
+    }
+
+    return () => stopAutoPlay()
+  }, [isAutoPlayStoped, focusedMovieIndex, movies, setFocusedMovie])
 
   const hendleBackButtonPress = () => {
+    setIsAutoPlayStoped(true)
     focusedMovieIndex > 0
       ? setFocusedMovie(movies[focusedMovieIndex - 1])
       : setFocusedMovie(movies[movies.length - 1])
   }
 
   const hendleForwardButtonPress = () => {
+    setIsAutoPlayStoped(true)
     focusedMovieIndex < movies.length - 1
       ? setFocusedMovie(movies[focusedMovieIndex + 1])
       : setFocusedMovie(movies[0])
@@ -56,6 +62,7 @@ export const MainSlider: React.FC<Props> = ({ movies, focusedMovie, setFocusedMo
           <StyledCard
             src={imageApiUrl + movie.poster_path}
             onClick={() => {
+              setIsAutoPlayStoped(true)
               setFocusedMovie(movie)
             }}
             key={key}
